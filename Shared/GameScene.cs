332ce21c639e05e5637a953e2a4fb535e19a507f
ksoloway <35354804+ksoloway@users.noreply.github.com>
2018-11-12 23:47:18 -0800

    Add files via upload
    Final game minus algorithm for results

--- Shared/GameScene.cs
+++ Shared/GameScene.cs
@@ -21,16 +21,22 @@ namespace SpriteKitGame
         SKSpriteNode card4 = SKSpriteNode.FromImageNamed(NSBundle.MainBundle.PathForResource("card4", "png"));
         SKSpriteNode start = SKSpriteNode.FromImageNamed(NSBundle.MainBundle.PathForResource("start", "png"));
         SKSpriteNode instructions = SKSpriteNode.FromImageNamed(NSBundle.MainBundle.PathForResource("Instructions", "png"));
-
+        SKSpriteNode dragcardhere = SKSpriteNode.FromImageNamed(NSBundle.MainBundle.PathForResource("dragcardhere", "png"));
+        SKSpriteNode gameOver = SKSpriteNode.FromImageNamed(NSBundle.MainBundle.PathForResource("Game-over-pixels", "png"));
+        CGPoint dragCardHerePos;
+        SKLabelNode moneytitle, cardlabel, moneylabel, cardtitle;
+        Boolean rcard1,rcard2, rcard3, rcard4,move1,move2,move3,move4;
         //arrays that correspond with each card deck
+        int counter1, counter2, counter3, counter4, turn = 0;
+        int totalMoney = 2000;
         int[] arr1 = new int[40] { 0, 0, 150, 0, 300, 0, 200, 0, 250, 350, 0, 350, 0, 250, 200, 0, 300, 150, 0, 0, 0,
-            300, 0, 350, 0, 200, 250, 150, 0, 0, 350, 200, 250, 0, 0, 0, 150, 300, 0, 0 };
+            300, 0, 350, 0, 200, 250, 150, 0, 0, 350, 200, 250, 0, 0, 0, 150, 300, 0, 0 }; //+100
         int[] arr2 = new int[40] { 0, 0, 0, 0, 0, 0, 0, 0, 1250, 0, 0, 0, 0, 1250, 0, 0, 0, 0, 0, 0, 1250, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 1250, 0, 0, 0, 0, 0, 0, 0, 0 };
+            0, 0, 0, 0, 0, 0, 0, 1250, 0, 0, 0, 0, 0, 0, 0, 0 };//+100
         int[] arr3 = new int[40] { 0, 0, 50, 0, 50, 0, 50, 0, 50, 50, 0, 25, 75, 0, 0, 0, 25, 75, 0, 50, 0, 0, 0,
-            50, 25, 50, 0, 0, 75, 50, 0, 0, 0, 25, 25, 0, 75, 0, 50, 75 };
+            50, 25, 50, 0, 0, 75, 50, 0, 0, 0, 25, 25, 0, 75, 0, 50, 75 };//+50
         int[] arr4 = new int[40] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 250, 0, 0, 0, 0, 0, 0, 0, 0, 0, 250,
-            0, 0, 0, 0, 0, 0, 0, 0, 250, 0, 0, 0, 0, 0, 250, 0, 0, 0, 0, 0 };
+            0, 0, 0, 0, 0, 0, 0, 0, 250, 0, 0, 0, 0, 0, 250, 0, 0, 0, 0, 0 };//+50
 
         int move = 0; //flag that detects which card deck is dragged from (card1 = 1, card2 = 2, ...)
 
@@ -41,6 +47,7 @@ namespace SpriteKitGame
         SKSpriteNode card4move = SKSpriteNode.FromImageNamed(NSBundle.MainBundle.PathForResource("card4", "png"));
 
         SKNode[] removelist = new SKSpriteNode[1]; //list that stores the node to be removed from scene
+        SKNode[] removelabellist = new SKLabelNode[1];
 
         protected GameScene(IntPtr handle) : base(handle)
         {
@@ -53,9 +60,11 @@ namespace SpriteKitGame
         public override void DidMoveToView(SKView view)
         {
             //set the button size and position
-            start.ScaleTo(new CGSize(width: 150, height: 150));
-            start.Position = new CGPoint(Frame.Width / 2.3, Frame.Height / 1.3);
+            start.ScaleTo(new CGSize(width: 500, height: 400));
+            start.Position = new CGPoint(Frame.Width / 2, Frame.Height / 2.5);
             AddChild(start); //add the node to the scene
+
+
         }
 
 #if __IOS__
@@ -80,7 +89,7 @@ namespace SpriteKitGame
                     removelist[0] = start;
                     RemoveChildren(removelist); //remove the start button and set up the instruction
 
-                    instructions.ScaleTo(new CGSize(width: Frame.Width / 2, height: Frame.Height / 2));
+                    instructions.ScaleTo(new CGSize(width: 375, height: 600));
                     instructions.Position = new CGPoint(Frame.Width / 2, Frame.Height / 2);
 
                     AddChild(instructions); //set up the instruction scene
@@ -109,6 +118,62 @@ namespace SpriteKitGame
                     card4.ScaleTo(new CGSize(width: 150, height: 150));
                     card4.Position = new CGPoint(Frame.Width / 1.7, Frame.Height / 2.2);
                     AddChild(card4);
+
+                    dragcardhere.ScaleTo(new CGSize(width: 150, height: 150));
+                    dragcardhere.Position = new CGPoint(Frame.Width / 1.97, Frame.Height / 5.1);
+                    AddChild(dragcardhere);
+
+                    moneytitle = new SKLabelNode("Arial")
+                    {
+                        Text = "Total money: ",
+                        FontSize = 18,
+                        Position = new CGPoint(Frame.Width / 2.7, Frame.Height / 5.1)
+
+                    };
+
+                    cardtitle = new SKLabelNode("Arial")
+                    {
+                        Text = "Last card value: ",
+                        FontSize = 18,
+                        Position = new CGPoint(Frame.Width / 1.55, Frame.Height / 5.1)
+
+                    };
+
+                    moneylabel = new SKLabelNode("Arial")
+                    {
+                        Text = totalMoney.ToString(),
+                        FontSize = 18,
+                        Position = new CGPoint(Frame.Width / 2.7, Frame.Height / 6)
+
+                    };
+
+                    cardlabel = new SKLabelNode("Arial")
+                    {
+                        Text = "",
+                        FontSize = 18,
+                        Position = new CGPoint(Frame.Width / 1.55, Frame.Height / 6)
+
+                    };
+
+                    AddChild(moneytitle);
+                    AddChild(cardtitle);
+                    AddChild(moneylabel);
+                    AddChild(cardlabel);
+                }
+                else if ((sprite = GetNodeAtPoint(location)).Equals(gameOver))
+                {
+                    counter1 = 0;
+                    counter2 = 0;
+                    counter3 = 0;
+                    counter4 = 0;
+                    turn = 0;
+                    totalMoney = 2000;
+                    removelist[0] = gameOver;
+                    RemoveChildren(removelist);
+                    cardlabel.Text = "";
+                    start.ScaleTo(new CGSize(width: 500, height: 400));
+                    start.Position = new CGPoint(Frame.Width / 2, Frame.Height / 2.5);
+                    AddChild(start);
                 }
                 else // logic for card decks when they are touched
                 {
@@ -118,9 +183,15 @@ namespace SpriteKitGame
                         removelist[0] = card1move;
                         RemoveChildren(removelist);
                         move = 1; //toggle the flag to 1 for TouhesMoved and Ended
+                        if(counter1 == 39){
+                            removelist[0] = card1;
+                            RemoveChildren(removelist);
+                            rcard1 = true;
+                        }
                         card1move.ScaleTo(new CGSize(width: 150, height: 150));
                         card1move.Position = new CGPoint(Frame.Width / 2.3, Frame.Height / 1.3);
                         AddChild(card1move); //add the moving card 1 
+                        move1 = true;
                     }
                     // card deck 2 (green)
                     else if ((sprite = GetNodeAtPoint(location)).Equals(card2) || sprite.Equals(card2move))
@@ -128,9 +199,16 @@ namespace SpriteKitGame
                         removelist[0] = card2move;
                         RemoveChildren(removelist);
                         move = 2;
+                        if (counter2 == 39)
+                        {
+                            removelist[0] = card2;
+                            RemoveChildren(removelist);
+                            rcard2 = true;
+                        }
                         card2move.ScaleTo(new CGSize(width: 150, height: 150));
                         card2move.Position = new CGPoint(Frame.Width / 1.7, Frame.Height / 1.3);
                         AddChild(card2move);
+                        move2 = true;
                     }
                     // card deck 3 (blue)
                     else if ((sprite = GetNodeAtPoint(location)).Equals(card3) || sprite.Equals(card3move))
@@ -138,9 +216,16 @@ namespace SpriteKitGame
                         removelist[0] = card3move;
                         RemoveChildren(removelist);
                         move = 3;
+                        if (counter3 == 39)
+                        {
+                            removelist[0] = card3;
+                            RemoveChildren(removelist);
+                            rcard3 = true;
+                        }
                         card3move.ScaleTo(new CGSize(width: 150, height: 150));
                         card3move.Position = new CGPoint(Frame.Width / 2.3, Frame.Height / 2.2);
                         AddChild(card3move);
+                        move3 = true;
                     }
                     // card deck 4 (purple)
                     else if ((sprite = GetNodeAtPoint(location)).Equals(card4) || sprite.Equals(card4move))
@@ -148,9 +233,16 @@ namespace SpriteKitGame
                         removelist[0] = card4move;
                         RemoveChildren(removelist);
                         move = 4;
+                        if (counter4 == 39)
+                        {
+                            removelist[0] = card4;
+                            RemoveChildren(removelist);
+                            rcard4 = true;
+                        }
                         card4move.ScaleTo(new CGSize(width: 150, height: 150));
                         card4move.Position = new CGPoint(Frame.Width / 1.7, Frame.Height / 2.2);
                         AddChild(card4move);
+                        move4 = true;
                     }
                     else
                     {
@@ -195,42 +287,169 @@ namespace SpriteKitGame
         public override void TouchesEnded(NSSet touches, UIEvent evt)
         {
             var moveDest = new CGPoint(); //the destination of the movement
+
             foreach (var touch in touches)
             {
 
                 // logic for different moving card based on the move flag
                 switch (move)
                 {
+
                     case 1:
-                        moveDest = new CGPoint(Frame.Width / 2.3, Frame.Height / 1.3);
 
-                        card1move.RunAction(SKAction.MoveTo(moveDest, 0.2));
+                        CGPoint card1Pos = card1move.Position;
+                        dragCardHerePos = dragcardhere.Position;
+                        if (card1Pos.X < dragCardHerePos.X + 75.0 && card1Pos.X > dragCardHerePos.X - 75.0 &&
+                           card1Pos.Y < dragCardHerePos.Y + 75.0 && card1Pos.Y > dragCardHerePos.Y - 75.0)
+                        {
+                            totalMoney+= -arr1[counter1]+100;
+                            cardlabel.Text = (-arr1[counter1] + 100).ToString();
+                            moneylabel.Text = totalMoney.ToString();
+                            counter1++;
+                            turn++;
+                            removelist[0] = card1move;
+                            RemoveChildren(removelist);
 
-                        removelist[0] = card1move;
+                        }
+                        else
+                        {
+                            moveDest = new CGPoint(Frame.Width / 2.3, Frame.Height / 1.3);
+
+
+                            card1move.RunAction(SKAction.MoveTo(moveDest, 0.2));
+
+                            removelist[0] = card1move;
+                        }
                         break;
                     case 2:
-                        moveDest = new CGPoint(Frame.Width / 1.7, Frame.Height / 1.3);
+                        CGPoint card2Pos = card2move.Position;
+                        dragCardHerePos = dragcardhere.Position;
+                        if (card2Pos.X < dragCardHerePos.X + 75.0 && card2Pos.X > dragCardHerePos.X - 75.0 &&
+                           card2Pos.Y < dragCardHerePos.Y + 75.0 && card2Pos.Y > dragCardHerePos.Y - 75.0)
+                        {
+                            totalMoney += -arr2[counter2] + 100;
+                            cardlabel.Text = (-arr2[counter2] + 100).ToString();
+                            moneylabel.Text = totalMoney.ToString();
+                            counter2++;
+                            turn++;
+                            removelist[0] = card2move;
+                            RemoveChildren(removelist);
+                        }
+                        else
+                        {
+                            moveDest = new CGPoint(Frame.Width / 1.7, Frame.Height / 1.3);
 
-                        card2move.RunAction(SKAction.MoveTo(moveDest, 0.2));
+                            card2move.RunAction(SKAction.MoveTo(moveDest, 0.2));
 
-                        removelist[0] = card2move;
+                            removelist[0] = card2move;
+                        }
                         break;
                     case 3:
-                        moveDest = new CGPoint(Frame.Width / 2.3, Frame.Height / 2.2);
+                        CGPoint card3Pos = card3move.Position;
+                        dragCardHerePos = dragcardhere.Position;
+                        if (card3Pos.X < dragCardHerePos.X + 75.0 && card3Pos.X > dragCardHerePos.X - 75.0 &&
+                           card3Pos.Y < dragCardHerePos.Y + 75.0 && card3Pos.Y > dragCardHerePos.Y - 75.0)
+                        {
+                            totalMoney += -arr3[counter3] + 50;
+                            cardlabel.Text = (-arr3[counter3] + 50).ToString();
+                            moneylabel.Text = totalMoney.ToString();
+                            counter3++;
+
+                            turn++;
+                            removelist[0] = card3move;
+                            RemoveChildren(removelist);
+                        }
+                        else
+                        {
+                            moveDest = new CGPoint(Frame.Width / 2.3, Frame.Height / 2.2);
 
 
-                        card3move.RunAction(SKAction.MoveTo(moveDest, 0.2));
+                            card3move.RunAction(SKAction.MoveTo(moveDest, 0.2));
 
-                        removelist[0] = card3move;
+                            removelist[0] = card3move;
+                        }
                         break;
                     case 4:
-                        moveDest = new CGPoint(Frame.Width / 1.7, Frame.Height / 2.2);
-                        card4move.RunAction(SKAction.MoveTo(moveDest, 0.2));
+                        CGPoint card4Pos = card4move.Position;
+                        dragCardHerePos = dragcardhere.Position;
+                        if (card4Pos.X < dragCardHerePos.X + 75.0 && card4Pos.X > dragCardHerePos.X - 75.0 &&
+                           card4Pos.Y < dragCardHerePos.Y + 75.0 && card4Pos.Y > dragCardHerePos.Y - 75.0)
+                        {
+                            totalMoney += -arr4[counter4] + 50;
+                            cardlabel.Text = (-arr4[counter4] + 50).ToString();
+                            moneylabel.Text = totalMoney.ToString();
+                            counter4++;
+                            turn++;
+                            removelist[0] = card4move;
+                            RemoveChildren(removelist);
+                        }
+                        else
+                        {
+                            moveDest = new CGPoint(Frame.Width / 1.7, Frame.Height / 2.2);
+                            card4move.RunAction(SKAction.MoveTo(moveDest, 0.2));
 
+                            removelist[0] = card4move;
+                        }
+                        break;
+                   
+
+
+                }
+                if (turn == 4)
+                {
+                    if(move1){
+                        removelist[0] = card1move;
+                        RemoveChildren(removelist);
+                    } 
+                    if (move2)
+                    {
+                        removelist[0] = card2move;
+                        RemoveChildren(removelist);
+                    } 
+                    if (move3)
+                    {
+                        removelist[0] = card3move;
+                        RemoveChildren(removelist);
+                    } 
+                    if (move4)
+                    {
                         removelist[0] = card4move;
-                        break;
-                    default:
-                        break;
+                        RemoveChildren(removelist);
+                    }
+                    if (!rcard1)
+                    {
+                        removelist[0] = card1;
+                        RemoveChildren(removelist);
+                    }
+                    if (!rcard2)
+                    {
+                        removelist[0] = card2;
+                        RemoveChildren(removelist);
+                    }
+                    if (!rcard3)
+                    {
+                        removelist[0] = card3;
+                        RemoveChildren(removelist);
+                    }
+                    if (!rcard4)
+                    {
+                        removelist[0] = card4;
+                        RemoveChildren(removelist);
+                    }
+                    removelist[0] = dragcardhere;
+                    RemoveChildren(removelist);
+
+                    removelabellist[0] = moneylabel;
+                    RemoveChildren(removelabellist);
+                    removelabellist[0] = moneytitle;
+                    RemoveChildren(removelabellist);
+                    removelabellist[0] = cardtitle;
+                    RemoveChildren(removelabellist);
+                    removelabellist[0] = cardlabel;
+                    RemoveChildren(removelabellist);
+                    gameOver.ScaleTo(new CGSize(width: 375, height: 650));
+                    gameOver.Position = new CGPoint(Frame.Width / 2, Frame.Height / 2);
+                    AddChild(gameOver);
                 }
             }
 
